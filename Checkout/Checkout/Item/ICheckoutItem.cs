f15b759067651112decacf29e8376d2e1b364aa0
Liam Griffiths <rockinliam@gmail.com>
2019-11-04 16:03:40 +0000

    Checkout Item created.

---
+++ Checkout/Checkout/Item/ICheckoutItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checkout
+{
+    interface ICheckoutItem
+    {
+        string Name
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Checkout/Checkout/Item/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checkout
+{
+    public class Item : ICheckoutItem
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
