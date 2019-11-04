0dce5a6978fc07dfb5fb648b16bf2f1b38eb537b
Liam Griffiths <rockinliam@gmail.com>
2019-11-04 15:45:39 +0000

    init project

---
+++ Checkout/Checkout/ICheckout.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checkout
+{
+    interface ICheckout
+    {
+        void Scan(Item item);
+        int Total();
+    }
+}
